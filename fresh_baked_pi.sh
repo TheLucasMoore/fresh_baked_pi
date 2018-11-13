445694d871bb8bccd431d81f603696ff5efee6cb
Lucas Moore <hey@thelucasmoore.com>
2018-11-12 23:47:07 -0700

    the script exits, try the stuff right from the docs

--- fresh_baked_pi.sh
+++ fresh_baked_pi.sh
@@ -2,12 +2,17 @@
 # see https://vicpimakers.ca/tutorials/raspbian/change-the-raspbian-root-password/
 # Is that a great idea? No. Does it simplify things? You betchya.
 
-# This is essentially a reconfiguration of the keyboard with
-# $ dpkg-reconfigure keyboard-configuration
-# the BTC 9116URF one is 87 keys and seems to work just fine.
+# TO USE THIS SCRIPT RUN
+# wget https://raw.githubusercontent.com/TheLucasMoore/fresh_baked_pi/master/fresh_baked_pi.sh
+# chmod +x fresh_baked_pi.sh
+# /.fresh_baked_pi.sh
+
 # @note comment this out if you're copying my script, ok? I need this to get pipe | to work
 echo "################### WELCOME ###################"
 echo "First things first. Fix up that keyboard, yo."
+# This is essentially a reconfiguration of the keyboard with
+# $ dpkg-reconfigure keyboard-configuration
+# the BTC 9116URF one is 87 keys and seems to work just fine.
 wget https://raw.githubusercontent.com/TheLucasMoore/fresh_baked_pi/master/keyboard etc/default/keyboard
 echo "Restarting your keyboard configuration."
 service keyboard-setup restart
@@ -19,9 +24,7 @@ sudo apt-get install git zsh vim tmux;
 
 echo "################### Step Two ###################"
 echo "Great. Now let's get zsh installed."
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
-chmod +x ./install.sh
-./install.sh # shell script inception
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 # ZSH packages that I like.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
