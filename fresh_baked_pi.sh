9c2003bb62bbe2d5ab2d4c409e2813f631265fd4
Lucas Moore <hey@thelucasmoore.com>
2018-11-12 23:24:18 -0700

    add a bit of a prompt

--- fresh_baked_pi.sh
+++ fresh_baked_pi.sh
@@ -6,15 +6,18 @@
 # $ dpkg-reconfigure keyboard-configuration
 # the BTC 9116URF one is 87 keys and seems to work just fine.
 # @note comment this out if you're copying my script, ok? I need this to get pipe | to work
+echo "################### WELCOME ###################"
 echo "First things first. Fix up that keyboard, yo."
 wget https://raw.githubusercontent.com/TheLucasMoore/fresh_baked_pi/master/keyboard etc/default/keyboard
 echo "Restarting your keyboard configuration."
 service keyboard-setup restart
 
+echo "################### Step One ###################"
 echo "Let's upgrade everything here and install some nice packages."
 sudo apt-get update && sudo apt-get upgrade;
 sudo apt-get install git zsh vim tmux;
 
+echo "################### Step Two ###################"
 echo "Great. Now let's get zsh installed."
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
 chmod +x ./install.sh
@@ -22,8 +25,9 @@ chmod +x ./install.sh
 # ZSH packages that I like.
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# set my zshrc up nice and pretty
+# set zshrc up nice and pretty
 wget https://raw.githubusercontent.com/TheLucasMoore/fresh_baked_pi/master/zshrc ~/.zshrc
-source ./zshrc
+source ~/.zshrc
 
+echo "################### Step Three ###################"
 echo "Ok, things are pretty well set up on this machine now."
